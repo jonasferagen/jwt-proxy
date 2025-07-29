38c54a081602551f54eff626b837e79235d6376a
Jonas Feragen <jonas.feragen@gmail.com>
2025-07-29 18:38:55 +0200

    WIP

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('{*any}', async (req, res) => {
   try {
     const response = await axios.get(`${WC_API}${req.path}`, {
       params: req.query,
+      headers: {
+        'Cookie': req.headers.cookie || '',
+        'User-Agent': req.headers['user-agent'],
+        'X-Forwarded-For': req.ip,
+      },
       auth: {
         username: CK,
         password: CS
@@ -29,9 +34,21 @@ app.get('{*any}', async (req, res) => {
 
 app.post('{*any}', async (req, res) => {
 
+  console.log('--- PROXY POST REQUEST ---');
+  console.log('Path:', req.path);
+  console.log('Body:', JSON.stringify(req.body, null, 2));
+  console.log('Query:', req.query);
+  console.log('Headers:', JSON.stringify(req.headers, null, 2));
+  console.log('--------------------------');
+
   try {
     const response = await axios.post(`${WC_API}${req.path}`, req.body, {
       params: req.query,
+      headers: {
+        'Cookie': req.headers.cookie || '',
+        'User-Agent': req.headers['user-agent'],
+        'X-Forwarded-For': req.ip,
+      },
       auth: {
         username: CK,
         password: CS
